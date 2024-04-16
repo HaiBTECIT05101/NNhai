ec8db5ce54add18ef1ff17b1af0d2d62adaf83df
HaiBTECIT05101 <134904926+HaiBTECIT05101@users.noreply.github.com>
2024-04-16 08:03:48 +0700

    Add files via upload

---
+++ BookingActivity.java
@@ -0,0 +1,61 @@
+package com.example.vdtea.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vdtea.R;
+import com.example.vdtea.adapter.BookingAdapter;
+import com.example.vdtea.model.Booking;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BookingActivity extends AppCompatActivity {
+
+    BookingAdapter bookingAdapter;
+    RecyclerView recyclerView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser user = mAuth.getCurrentUser();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+        recyclerView = findViewById(R.id.bookingrcv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FirebaseRecyclerOptions<Booking> bookingOptions =
+                new FirebaseRecyclerOptions.Builder<Booking>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("booking").child(user.getUid()), Booking.class)
+                        .build();
+        bookingAdapter = new BookingAdapter(bookingOptions,this);
+        bookingAdapter.startListening();
+        recyclerView.setAdapter(bookingAdapter);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("My order");
+        toolbar.setNavigationIcon(R.drawable.back);
+        toolbar.setNavigationOnClickListener(v -> {
+            Intent intent = new Intent(BookingActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+        bottom_navigation();
+    }
+    private void bottom_navigation() {
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+        LinearLayout cartBtn = findViewById(R.id.cartBtn);
+        LinearLayout orderBtn = findViewById(R.id.orderBtn);
+        LinearLayout profileBtn = findViewById(R.id.profileBtn);
+
+        homeBtn.setOnClickListener(v -> startActivity(new Intent(BookingActivity.this, MainActivity.class)));
+        cartBtn.setOnClickListener(v -> startActivity(new Intent(BookingActivity.this, CartActivity.class)));
+        orderBtn.setOnClickListener(v -> startActivity(new Intent(BookingActivity.this, BookingActivity.class)));
+        profileBtn.setOnClickListener(v -> startActivity(new Intent(BookingActivity.this, UsersActivity.class)));
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.vdtea.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.vdtea.R;
+import com.example.vdtea.adapter.MainAdapter;
+import com.example.vdtea.model.Drinks;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.FirebaseDatabase;
+import static android.content.ContentValues.TAG;
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    MainAdapter mainAdapter;
+
+    EditText search;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser user = mAuth.getCurrentUser();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(user == null){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            String email = user.getEmail();
+            int atIndex = email.indexOf('@');
+            if (atIndex != -1) {
+                String username = email.substring(0,atIndex);
+
+                UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(username)
+                        .build();
+                user.updateProfile(profileUpdate)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "Update username successfully.");
+                                }
+                            }
+                        });
+
+            } else {
+                System.out.println("There is no '@' character in the string.");
+            }
+        }
+
+
+        recyclerView = findViewById(R.id.rcv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FirebaseRecyclerOptions<Drinks> options =
+                new FirebaseRecyclerOptions.Builder<Drinks>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("drinks"), Drinks.class)
+                        .build();
+
+        mainAdapter = new MainAdapter(options);
+        mainAdapter.startListening();
+        recyclerView.setAdapter(mainAdapter);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("List of products");
+        toolbar.setTitleCentered(true);
+        bottom_navigation();
+        search = findViewById(R.id.edt_search_name);
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                processsearch(s.toString());
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                processsearch(s.toString());
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                processsearch(s.toString());
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mainAdapter.startListening();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainAdapter.stopListening();
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.searchmenu, menu);
+//        MenuItem item = menu.findItem(R.id.search);
+//        SearchView searchView = (SearchView)item.getActionView();
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                processsearch(s);
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String s) {
+//                processsearch(s);
+//                return false;
+//            }
+//        });
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+
+
+    private void bottom_navigation() {
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+        LinearLayout cartBtn = findViewById(R.id.cartBtn);
+        LinearLayout orderBtn = findViewById(R.id.orderBtn);
+        LinearLayout profileBtn = findViewById(R.id.profileBtn);
+
+        homeBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainActivity.class)));
+        cartBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CartActivity.class)));
+        orderBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, BookingActivity.class)));
+        profileBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, UsersActivity.class)));
+    }
+    private void processsearch(String s) {
+        FirebaseRecyclerOptions<Drinks> options =
+                new FirebaseRecyclerOptions.Builder<Drinks>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("drinks").orderByChild("drinks_name").startAt(s).endAt(s+"\uf8ff"), Drinks.class)
+                        .build();
+        mainAdapter = new MainAdapter(options);
+        mainAdapter.startListening();
+        recyclerView.setAdapter(mainAdapter);
+    }
+}
